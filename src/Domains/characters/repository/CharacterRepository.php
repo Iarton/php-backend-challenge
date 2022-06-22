1e99afc8e7a0a6bda118f77d8e820aad83e36164
Iarton <amarcelobrasil@gmail.com>
2022-06-22 07:29:53 -0300

    feat: Add Request Helper to clean redundant code

--- src/Domains/characters/repository/CharacterRepository.php
+++ src/Domains/characters/repository/CharacterRepository.php
@@ -2,57 +2,38 @@
 
 namespace Character\repository;
 
+use Helper\RequestHelper;
+
+include_once  __DIR__ . '/../../../helper/RequestHelper.php';
+
 class CharacterRepository
 {
     public function createCharacter($character): int
     {
-        $client = new \GuzzleHttp\Client();
-        $response =  $client->post('https://backend-challenge.hasura.app/v1/graphql', [
-            'headers' => [
-                'x-hasura-admin-secret' => 'uALQXDLUu4D9BC8jAfXgDBWm1PMpbp0pl5SQs4chhz2GG14gAVx5bfMs4I553keV',
-                'content-type' => 'application/json'
-            ],
-            'json' => [
-                'query' => "mutation CreateCharacter {\n  insert_Character(objects:" .
-                    "{name: \"" . $character['name'] . "\"" .
-                    "image_url: \"" . $character['image_url'] .
-                    "\"}) {\n    returning {\n      id\n    }\n  }\n}",
-                "operationName" => "CreateCharacter"
-            ]
-        ]);
 
-        $data = json_decode($response->getBody(), true);
+        $query = "mutation CreateCharacter {\n  insert_Character(objects:" .
+            "{name: \"" . $character['name'] . "\"" .
+            "image_url: \"" . $character['image_url'] .
+            "\"}) {\n    returning {\n      id\n    }\n  }\n}";
+
+        $request = new RequestHelper();
+        $data = $request->sendRepositoryRequest($query, "CreateCharacter");
+
         return  $data['data']['insert_Character']['returning'][0]['id'];
     }
 
     public function addQuote(int $characterId, string $quote): void
     {
-        $client = new \GuzzleHttp\Client();
-        $client->post('https://backend-challenge.hasura.app/v1/graphql', [
-            'headers' => [
-                'x-hasura-admin-secret' => 'uALQXDLUu4D9BC8jAfXgDBWm1PMpbp0pl5SQs4chhz2GG14gAVx5bfMs4I553keV',
-                'content-type' => 'application/json'
-            ],
-            'json' => [
-                'query' => "mutation CreateQuote {\n  insert_Quote(objects: {text: \"" . $quote
-                    . "\", character_id: " . $characterId . "}) {\n    returning {\n      id\n      text\n    }\n  }\n}\n",
-                "operationName" => "CreateQuote"
-            ]
-        ]);
+        $query = "mutation CreateQuote {\n  insert_Quote(objects: {text: \"" . $quote
+            . "\", character_id: " . $characterId . "}) {\n    returning {\n      id\n      text\n    }\n  }\n}\n";
+        $request = new RequestHelper();
+        $request->sendRepositoryRequest($query, "CreateQuote");
     }
 
     public function deleteAll(): void
     {
-        $client = new \GuzzleHttp\Client();
-        $client->post('https://backend-challenge.hasura.app/v1/graphql', [
-            'headers' => [
-                'x-hasura-admin-secret' => 'uALQXDLUu4D9BC8jAfXgDBWm1PMpbp0pl5SQs4chhz2GG14gAVx5bfMs4I553keV',
-                'content-type' => 'application/json'
-            ],
-            'json' => [
-                'query' => "mutation DeleteAll {\n  delete_Character(where: {id: {_gt: 0}}) {\n    affected_rows\n  }\n}\n",
-                "operationName" => "DeleteAll"
-            ]
-        ]);
+        $query = "mutation DeleteAll {\n  delete_Character(where: {id: {_gt: 0}}) {\n    affected_rows\n  }\n}\n";
+        $request = new RequestHelper();
+        $request->sendRepositoryRequest($query, "DeleteAll");
     }
 }
---
+++ src/helper/RequestHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Helper;
+
+class RequestHelper
+{
+    public function sendRepositoryRequest(string $query, string $operation = null): array
+    {
+        $json = ['query' => $query];
+        if ($operation != null) {
+            $json[] = ["operationName" => $operation];
+        }
+        $client = new \GuzzleHttp\Client();
+        $response =  $client->post('https://backend-challenge.hasura.app/v1/graphql', [
+            'headers' => [
+                'x-hasura-admin-secret' => 'uALQXDLUu4D9BC8jAfXgDBWm1PMpbp0pl5SQs4chhz2GG14gAVx5bfMs4I553keV',
+                'content-type' => 'application/json'
+            ],
+            'json' => $json
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+}
